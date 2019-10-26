d1940b79b258e29375369d636d836e8ce6bb2edd
Elias Hall <elias.hall@gmail.com>
2019-10-26 17:10:05 -0500

    Initial Commit

---
+++ Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tourist
+//
+//  Created by Elias Hall on 10/26/19.
+//  Copyright © 2019 Elias Hall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
